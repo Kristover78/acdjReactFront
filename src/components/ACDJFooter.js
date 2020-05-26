b2e3357996768931cc210d1d9ce13fffc7e731d3
Kristover <cni78.2017@actusoft.fr>
2020-05-26 10:37:15 +0200

    initial import

---
+++ src/components/ACDJFooter.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {MDBFooter} from "mdbreact";
+
+const ACDJFooter = () => {
+    return (
+        <MDBFooter color='indigo'>
+            <p className='footer-copyright mb-0 py-3 text-center'>
+                &copy; {new Date().getFullYear()} Copyright:
+                <a href='https://www.MDBootstrap.com'> MDBootstrap.com </a>
+            </p>
+        </MDBFooter>
+    )
+}
+
+export default ACDJFooter;
---
+++ src/components/HomePageSection1.js
@@ -0,0 +1,32 @@
+import React from "react";
+import SectionImage from '../assets/img/20160909_214748.jpg';
+import {MDBCol, MDBRow, MDBAnimation} from "mdbreact";
+
+class HomePageSection1 extends React.Component {
+    render() {
+        return (
+            <section className="mt-5 wow fadeIn">
+                <MDBRow>
+                    <MDBCol md="6" className="mb-4">
+                        <MDBAnimation reveal type='fadeIn'>
+                            <img src={SectionImage} className="img-fluid z-depth-1-half" alt=""/>
+                        </MDBAnimation>
+                    </MDBCol>
+                    <div className="col-md-6 mb-4">
+                        <h3 className="h3 mb-3">Trouver de nouveaux partenaires</h3>
+                        <p>Vous en avez marre de tourner en rond..., envie de <strong>nouveaux
+                            partenaires</strong> de jeu ? de découvrir des <strong>nouvelles
+                            stratégies</strong> auxquelles vous n'auriez même pas pensé ?</p>
+                        <p>Rien de mieux donc que de rejoindre une association qui pourra répondre à
+                            (presque) toutes vos attentes.
+                            Diversifier ainsi le challenge dans la joie et la bonne humeur.
+                            C'est l'occasion aussi de faire de nouvelles belles <strong>rencontres</strong>.
+                        </p>
+                    </div>
+                </MDBRow>
+            </section>
+        )
+    }
+}
+
+export default HomePageSection1;
