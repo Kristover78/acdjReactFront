cb8188ade4d4262f9a6eead709ec1677475c0e84
gituser <Capit@l78>
2021-03-21 13:33:42 +0100

    stable version. +pages release, contact, forum, mentions, +footer

--- src/services/Api/index.js
+++ src/services/Api/index.js
@@ -7,3 +7,12 @@ export function getInfosFromApiWithPage(currentPage) {
         .then((response) => response.json())
         .catch((error) => console.error(error));
 }
+
+export function getInfosFromApiWithPage(currentPage) {
+    const url =
+        'https://localhost:8082/api/event/all/' +
+        currentPage;
+    return fetch(url)
+        .then((response) => response.json())
+        .catch((error) => console.error(error));
+}
